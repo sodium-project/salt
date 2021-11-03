55412bad33cbf8ef6e4e35c5f69132df1877307a
Andrii <kovacz@ukr.net>
2021-11-03 02:05:38 +0200

    Fixed formatting issues

--- salt-core/salt/core/meta.hpp
+++ salt-core/salt/core/meta.hpp
@@ -9,19 +9,19 @@
 namespace salt {
 
 #if defined(SALT_STDLIB_HAS_NO_CONCEPTS)
-
+// clang-format off
 template <typename From, typename To>
 concept convertible_to =
             std::is_convertible_v<From, To>
          && requires(std::add_rvalue_reference_t<From> (&f__)()) {
     static_cast<To>(f__());
 };
-
+// clang-format on
 #else
-
+// clang-format off
 template <typename From, typename To>
 concept convertible_to = std::convertible_to<From, To>;
-
+// clang-format on
 #endif
 
 } // namespace salt
