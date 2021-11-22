51b465c75d65ca9edebd222c1bfc99fc04be9461
Andrii <kovacz@ukr.net>
2021-11-23 00:28:27 +0200

    Fixed `SALT_ASSERT` macro

--- salt-config/salt/config/assert.hpp
+++ salt-config/salt/config/assert.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
-#define SALT_ASSERT(condition)                                                                                         \
-    if (!(condition)) {                                                                                                \
-        salt::error("Assertion '{}' failed.", SALT_TO_STRING(condition));                                              \
-    }
+#ifdef NDEBUG
+#    define SALT_ASSERT(condition) ((void)0)
+#else
+#    define SALT_ASSERT(condition)                                                                                     \
+        if (!(condition)) {                                                                                            \
+            salt::error("Assertion '{}' failed.", SALT_TO_STRING(condition));                                          \
+        }
+#endif
