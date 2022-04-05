e4273bb4b37c706dc035ded969de79a97cfdfb28
Andrii <kovacz@ukr.net>
2022-04-06 01:46:19 +0300

    Added `Uninitialized_storage` to `salt-utils` lib

---
+++ salt-utils/salt/utils/uninitialized_storage-test.cpp
@@ -0,0 +1,30 @@
+#include <catch2/catch.hpp>
+
+#include <salt/utils.hpp>
+
+struct [[nodiscard]] Dummy final {
+    int* pointer = nullptr;
+};
+
+TEST_CASE("salt::Uninitialized_storage", "[salt-utils/uninitialized_storage.hpp]") {
+    SECTION("it can be constructed from fundamental type") {
+        auto storage = salt::Uninitialized_storage_for<int>{};
+        storage.construct<int>(2022);
+
+        auto expected_value = int{2022};
+        REQUIRE(expected_value == *storage.get<int>());
+
+        storage.destruct<int>();
+    }
+
+    SECTION("it can be constructed from user-defined type") {
+        auto expected_value = int{2022};
+
+        auto storage = salt::Uninitialized_storage_for<Dummy>{};
+        storage.construct<Dummy>(Dummy{&expected_value});
+
+        REQUIRE(expected_value == *(*storage.get<Dummy>()).pointer);
+
+        storage.destruct<Dummy>();
+    }
+}
---
+++ salt-utils/salt/utils/uninitialized_storage.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <salt/utils/detail/aligned_cast.hpp>
+
+namespace salt {
+
+using std::constructible_from;
+using std::destructible;
+
+template <std::size_t Size, std::size_t Alignment>
+struct [[nodiscard]] Uninitialized_storage final {
+    // clang-format off
+    template <typename T, typename... Args>
+    requires sized             <T, Size     >
+          && aligned           <T, Alignment>
+          && constructible_from<T, Args...  >
+    T& construct(Args&&... args) noexcept(std::is_nothrow_constructible_v<T, Args...>) {
+        auto const storage = get<T>();
+        ::new (storage) T{std::forward<Args>(args)...};
+        return *storage;
+    }
+    // clang-format on
+
+    template <destructible T> void destruct() noexcept {
+        get<T>()->~T();
+    }
+
+    template <aligned_as_pow2 T> [[nodiscard]] T* get() noexcept {
+        return detail::aligned_cast<T*>(addressof(storage_));
+    }
+
+    template <aligned_as_pow2 T> [[nodiscard]] T const* get() const noexcept {
+        return detail::aligned_cast<T const*>(addressof(storage_));
+    }
+
+private:
+    alignas(Alignment) std::byte storage_[Size];
+};
+
+template <typename T>
+using Uninitialized_storage_for = Uninitialized_storage<sizeof(T), alignof(T)>;
+
+} // namespace salt
