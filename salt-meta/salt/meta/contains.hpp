f5d49097e87596450be6b2677d6931666c567842
Andrii <kovacz@ukr.net>
2022-07-26 03:39:23 +0300

    Added `contains` concept

--- salt-meta/salt/meta/contains.hpp
+++ salt-meta/salt/meta/contains.hpp
@@ -2,10 +2,15 @@
 
 namespace salt {
 
+namespace detail {
 template <typename T, typename... Ts>
 struct [[nodiscard]] contains : std::disjunction<std::is_same<T, Ts>...> {};
+} // namespace detail
 
 template <typename T, typename... Ts>
-static constexpr inline bool contains_v = contains<T, Ts...>::value;
+static constexpr inline bool contains_v = detail::contains<T, Ts...>::value;
+
+template <typename T, typename... Ts>
+concept contains = contains_v<T, Ts...>;
 
 } // namespace salt
