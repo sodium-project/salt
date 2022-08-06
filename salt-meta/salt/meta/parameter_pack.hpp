6ecdbc302d395bdddfbc54f9e5b58e39d39395e6
Andrii <kovacz@ukr.net>
2022-08-06 22:45:29 +0300

    Added the ability to get the type by index

--- salt-meta/salt/meta/parameter_pack.hpp
+++ salt-meta/salt/meta/parameter_pack.hpp
@@ -2,10 +2,15 @@
 
 namespace salt {
 
+// clang-format off
 template <typename... Args>
 struct [[nodiscard]] parameter_pack final {
+    using types = std::tuple<Args...>;
     template <template <typename...> typename T, typename... Ts>
     using apply = T<Ts..., Args...>;
+    template <std::size_t I>
+    using get = std::tuple_element_t<I, std::tuple<Args...>>;
 };
+// clang-format on
 
 } // namespace salt
