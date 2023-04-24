cef4738315e3d473d062e8165c919ea23e7bfd28
wander <kovacz@ukr.net>
2023-04-24 23:56:57 +0300

    Added more appropriate `Linux` detection

--- salt-config/salt/config/os_support.hpp
+++ salt-config/salt/config/os_support.hpp
@@ -3,6 +3,7 @@
 #define SALT_TARGET_APPLE   (0)
 #define SALT_TARGET_MACOSX  (0)
 #define SALT_TARGET_WINDOWS (0)
+#define SALT_TARGET_LINUX   (0)
 
 // clang-format off
 #if defined(_WIN64)
@@ -16,6 +17,9 @@
 #        undef  SALT_TARGET_MACOSX
 #        define SALT_TARGET_MACOSX (1)
 #    endif
+#elif defined(__linux__)
+#    undef  SALT_TARGET_LINUX
+#    define SALT_TARGET_LINUX (1)
 #endif
 // clang-format on
 
