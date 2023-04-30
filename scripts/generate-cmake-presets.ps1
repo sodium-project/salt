14d6c19ac077b377732fa05fe0efbd3d82bd740a
wander <kovacz@ukr.net>
2023-05-01 00:01:48 +0300

    Update scripts

--- scripts/generate-cmake-presets.ps1
+++ scripts/generate-cmake-presets.ps1
@@ -1,6 +1,6 @@
 Set-StrictMode -Version 2.0
 
-function generate_cmake_presets($os, $arch, $graphics) {
+function detect_clang() {
     if ([string]::IsNullOrEmpty($env:llvm)) {
         throw @"
         Cannot find a path to the 'Clang' compiler, it looks like it is not set in the `$env`
@@ -9,6 +9,20 @@ function generate_cmake_presets($os, $arch, $graphics) {
 "@
     }
 
+    $clang_version_major = Write-Output '__clang_major__'       | clang -E -x c - | Select-Object -Last 1
+    $clang_version_minor = Write-Output '__clang_minor__'       | clang -E -x c - | Select-Object -Last 1
+    $clang_version_patch = Write-Output '__clang_patchlevel__ ' | clang -E -x c - | Select-Object -Last 1
+
+    if ([int]$clang_version_major -lt 16) {
+        throw @"
+        The minimum supported version of Clang is 16 or higher. Please update Clang to the minimum supported version.
+"@
+    }
+
+    Write-Output "Detected clang version: $clang_version_major.$clang_version_minor.$clang_version_patch"
+}
+
+function generate_cmake_presets($os, $arch, $graphics) {
     $description = ""
     if ($graphics -ne "OpenGL") {
         $description = "This configuration is temporarily unsupported."
@@ -52,20 +66,24 @@ function generate_cmake_presets($os, $arch, $graphics) {
     return $presets
 }
 
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$cmake_presets = "$scriptPath/../CMakePresets.json"
+$(detect_clang)
+
+$project_root = split-path $MyInvocation.MyCommand.Definition | split-path -parent
+$cmake_presets = "$project_root\CMakePresets.json"
 
 if (Test-Path $cmake_presets) {
     Remove-Item $cmake_presets
 }
+
+Write-Output "Generating presets.."
 
 Add-Content -Path $cmake_presets -Value @"
 {
     "version": 3,
     "configurePresets": [
 $(generate_cmake_presets "windows" "x86_64" "OpenGL")
-$(generate_cmake_presets "windows" "x86_64" "DirectX")
 $(generate_cmake_presets "windows" "x86_64" "Vulkan")
+$(generate_cmake_presets "windows" "x86_64" "DirectX")
         {
             "name":        "base",
             "description": "For more information: http://aka.ms/cmakepresetsvs",
@@ -77,3 +95,5 @@ $(generate_cmake_presets "windows" "x86_64" "Vulkan")
     ]
 }
 "@
+
+Write-Output "Successfully generated presets in $cmake_presets"
--- scripts/generate-cmake-presets.sh
+++ scripts/generate-cmake-presets.sh
@@ -2,37 +2,54 @@
 source ${BASH_SOURCE[0]%/*}/common.sh
 
 #######################################################################################################################
-readonly NINJA_EXECUTABLE=`which ninja`
+# Detect target OS and Clang version.
 
-if [ "x${NINJA_EXECUTABLE}" == "x" ]; then
-    fail "Cannot find Ninja."
+detect_os() {
+    case "$OSTYPE" in
+        darwin*)  echo "MacOSX";;
+        linux*)   echo "Linux";;
+        msys*)    echo "Linux";;
+        *)        echo "Unsupported";;
+    esac
+}
+
+detect_clang() {
+    readonly CLANG=$(which clang)
+    if [ "x${CLANG}" == "x" ]; then
+        fail "Cannot find Clang. Please make sure it is available in your PATH."
+    fi
+
+    readonly CLANGXX=$(which clang++)
+    if [ "x${CLANGXX}" == "x" ]; then
+        fail "Cannot find Clang++. Please make sure it is available in your PATH."
+    fi
+
+    readonly CLANG_MAJOR=$(echo __clang_major__      | clang -E -x c - | tail -n 1)
+    readonly CLANG_MINOR=$(echo __clang_minor__      | clang -E -x c - | tail -n 1)
+    readonly CLANG_PATCH=$(echo __clang_patchlevel__ | clang -E -x c - | tail -n 1)
+
+    if [[ "${CLANG_MAJOR}" -lt 16 ]]; then
+        fail "The minimum supported version of Clang is 16 or higher. Please update Clang to the minimum supported version."
+    fi
+
+    echo "Detected clang version: ${CLANG_MAJOR}.${CLANG_MINOR}.${CLANG_PATCH}"
+}
+
+if [ "$(detect_os)" == "Unsupported" ]; then
+    fail "Unsupported OS: $OSTYPE."
+elif [ "$(detect_os)" == "Linux" ]; then    
+    detect_clang # For MacOS, the detection method will probably need to be revised.
 fi
 
 #######################################################################################################################
-readonly CLANG_EXECUTABLE=`which clang`
+readonly NINJA=$(which ninja)
 
-if [ "x${CLANG_EXECUTABLE}" == "x" ]; then
-    fail "Cannot find Clang."
-fi
-
-#######################################################################################################################
-readonly CLANGXX_EXECUTABLE=`which clang++`
-
-if [ "x${CLANGXX_EXECUTABLE}" == "x" ]; then
-    fail "Cannot find Clang++."
+if [ "x${NINJA}" == "x" ]; then
+    fail "Cannot find Ninja. Please make sure it is available in your PATH."
 fi
 
 #######################################################################################################################
 # Generate the CMakePresets.json
-
-# detect_os detects target OS.
-detect_os() {
-    case "$OSTYPE" in
-        darwin*)  echo "MacOSX";; 
-        linux*)   echo "Linux";;
-        *)        echo "Unsupported";;
-    esac
-}
 
 # apple_min_os_version returns the minimum supported version of the given OS/SDK.
 apple_min_os_version() {
@@ -45,15 +62,15 @@ apple_min_os_version() {
 
 # generate_apple_cmake_presets generates a portion of cmake-variants.yaml for the given OS and architectures.
 generate_apple_cmake_presets() {
-    OS=$1
-    GRAPHICS=$2
-    DESCRIPTION=$3
+    local readonly OS=$1
+    local readonly GRAPHICS=$2
+    local readonly DESCRIPTION=$3
 
     for ARCH in "${@:4}"; do
 cat <<EOF
         {
-            "name":         "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-debug",
-            "displayName":  "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-debug",
+            "name":         "${OS}-${ARCH}-$(lo ${GRAPHICS})-debug",
+            "displayName":  "${OS}-${ARCH}-$(lo ${GRAPHICS})-debug",
             "description":  "${DESCRIPTION}",
             "inherits":     "base",
             "architecture": {
@@ -61,7 +78,7 @@ cat <<EOF
                 "strategy": "external"
             },
             "cacheVariables": {
-                "CMAKE_MAKE_PROGRAM":           "${NINJA_EXECUTABLE}",
+                "CMAKE_MAKE_PROGRAM":           "${NINJA}",
                 "CMAKE_BUILD_TYPE":             "Debug",
                 "CMAKE_CXX_COMPILER":           "$(xcrun --sdk ${OS} --find clang++ 2>/dev/null)",
                 "CMAKE_C_COMPILER":             "$(xcrun --sdk ${OS} --find clang   2>/dev/null)",
@@ -74,8 +91,8 @@ cat <<EOF
             }
         },
         {
-            "name":         "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-release",
-            "displayName":  "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-release",
+            "name":         "${OS}-${ARCH}-$(lo ${GRAPHICS})-release",
+            "displayName":  "${OS}-${ARCH}-$(lo ${GRAPHICS})-release",
             "description":  "${DESCRIPTION}",
             "inherits":     "base",
             "architecture": {
@@ -83,7 +100,7 @@ cat <<EOF
                 "strategy": "external"
             },
             "cacheVariables": {
-                "CMAKE_MAKE_PROGRAM":           "${NINJA_EXECUTABLE}",
+                "CMAKE_MAKE_PROGRAM":           "${NINJA}",
                 "CMAKE_BUILD_TYPE":             "RelWithDebInfo",
                 "CMAKE_CXX_COMPILER":           "$(xcrun --sdk ${OS} --find clang++ 2>/dev/null)",
                 "CMAKE_C_COMPILER":             "$(xcrun --sdk ${OS} --find clang   2>/dev/null)",
@@ -101,18 +118,20 @@ EOF
 
 # generate_linux_cmake_presets generates a portion of cmake-variants.yaml for the given OS and architectures.
 generate_linux_cmake_presets() {
-    OS=$1
-    GRAPHICS=$2
-    DESCRIPTION=$3
+    local readonly OS=$1
+    local readonly GRAPHICS=$2
+    local readonly DESCRIPTION=$3
 
     for ARCH in "${@:4}"; do
+        # TODO:
+        #   It will be necessary to check the arm64 build for Linux. Let's skip that for now.
         if [[ "${ARCH}" == "arm64" ]]; then
             continue
         fi
 cat <<EOF
         {
-            "name":         "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-debug",
-            "displayName":  "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-debug",
+            "name":         "${OS}-${ARCH}-$(lo ${GRAPHICS})-debug",
+            "displayName":  "${OS}-${ARCH}-$(lo ${GRAPHICS})-debug",
             "description":  "${DESCRIPTION}",
             "inherits":     "base",
             "architecture": {
@@ -120,19 +139,19 @@ cat <<EOF
                 "strategy": "external"
             },
             "cacheVariables": {
-                "CMAKE_MAKE_PROGRAM":           "${NINJA_EXECUTABLE}",
+                "CMAKE_MAKE_PROGRAM":           "${NINJA}",
                 "CMAKE_BUILD_TYPE":             "Debug",
-                "CMAKE_CXX_COMPILER":           "${CLANGXX_EXECUTABLE}",
-                "CMAKE_C_COMPILER":             "${CLANG_EXECUTABLE}",
-                "CMAKE_OBJCXX_COMPILER":        "${CLANGXX_EXECUTABLE}",
-                "CMAKE_OBJC_COMPILER":          "${CLANG_EXECUTABLE}",
+                "CMAKE_CXX_COMPILER":           "${CLANGXX}",
+                "CMAKE_C_COMPILER":             "${CLANG}",
+                "CMAKE_OBJCXX_COMPILER":        "${CLANGXX}",
+                "CMAKE_OBJC_COMPILER":          "${CLANG}",
                 "CMAKE_OSX_ARCHITECTURES":      "${ARCH}",
                 "SALT_GRAPHICS":                "${GRAPHICS}"
             }
         },
         {
-            "name":         "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-release",
-            "displayName":  "${OS}-${ARCH}-$(tr '[:upper:]' '[:lower:]'<<<${GRAPHICS})-release",
+            "name":         "${OS}-${ARCH}-$(lo ${GRAPHICS})-release",
+            "displayName":  "${OS}-${ARCH}-$(lo ${GRAPHICS})-release",
             "description":  "${DESCRIPTION}",
             "inherits":     "base",
             "architecture": {
@@ -140,12 +159,12 @@ cat <<EOF
                 "strategy": "external"
             },
             "cacheVariables": {
-                "CMAKE_MAKE_PROGRAM":           "${NINJA_EXECUTABLE}",
+                "CMAKE_MAKE_PROGRAM":           "${NINJA}",
                 "CMAKE_BUILD_TYPE":             "Debug",
-                "CMAKE_CXX_COMPILER":           "${CLANGXX_EXECUTABLE}",
-                "CMAKE_C_COMPILER":             "${CLANG_EXECUTABLE}",
-                "CMAKE_OBJCXX_COMPILER":        "${CLANGXX_EXECUTABLE}",
-                "CMAKE_OBJC_COMPILER":          "${CLANG_EXECUTABLE}",
+                "CMAKE_CXX_COMPILER":           "${CLANGXX}",
+                "CMAKE_C_COMPILER":             "${CLANG}",
+                "CMAKE_OBJCXX_COMPILER":        "${CLANGXX}",
+                "CMAKE_OBJC_COMPILER":          "${CLANG}",
                 "CMAKE_OSX_ARCHITECTURES":      "${ARCH}",
                 "SALT_GRAPHICS":                "${GRAPHICS}"
             }
@@ -156,23 +175,22 @@ EOF
 
 # generate_cmake_presets generates a portion of cmake-variants.yaml for the given OS and architectures.
 generate_cmake_presets() {
-    OS=$1
-    GRAPHICS=$2
+    local readonly OS=$1
+    local readonly GRAPHICS=$2
 
+    local DESCRIPTION=""
     if [ "${GRAPHICS}" != "OpenGL" ]; then
-        DESCRIPTION="This configuration is temporarily unsupported."
+        DESCRIPTION+="This configuration is temporarily unsupported."
     fi
 
-    if [ "${OS}" == "MacOSX" ]; then        
-        generate_apple_cmake_presets macosx "${GRAPHICS}" "${DESCRIPTION}" "${@:3}"
-    elif [ "${OS}" == "Linux" ]; then
-        generate_linux_cmake_presets linux  "${GRAPHICS}" "${DESCRIPTION}" "${@:3}"
+    if [ "${OS}" == "MacOSX" ] && [ "${GRAPHICS}" != "Vulkan" ]; then
+        generate_apple_cmake_presets "$(lo ${OS})" "${GRAPHICS}" "${DESCRIPTION}" "${@:3}"
+    elif [ "${OS}" == "Linux" ] && [ "${GRAPHICS}" != "Metal" ]; then
+        generate_linux_cmake_presets "$(lo ${OS})" "${GRAPHICS}" "${DESCRIPTION}" "${@:3}"
     fi
 }
 
-if [ "$(detect_os)" == "Unsupported" ]; then
-    fail "Unsupported OS: $OSTYPE."
-fi
+echo "Generating presets.."
 
 # Will generate CMakePresets.json that is specific to your development environment, so that VSCode will be able
 # to configure VSCode CMake Tools correctly.
@@ -181,8 +199,8 @@ cat <<EOF >${PROJECT_ROOT}/CMakePresets.json
     "version": 3,
     "configurePresets": [
 $(generate_cmake_presets $(detect_os) OpenGL arm64 x86_64)
-$(generate_cmake_presets $(detect_os) Metal  arm64 x86_64)
 $(generate_cmake_presets $(detect_os) Vulkan arm64 x86_64)
+$(generate_cmake_presets $(detect_os) Metal  arm64 x86_64)
         {
             "name":         "base",
             "description":  "For more information: http://aka.ms/cmakepresetsvs",
@@ -194,3 +212,7 @@ $(generate_cmake_presets $(detect_os) Vulkan arm64 x86_64)
     ]
 }
 EOF
+
+echo "Successfully generated presets in ${PROJECT_ROOT}/CMakePresets.json"
+
+# code: language='Shell Script' insertSpaces=true tabSize=4
