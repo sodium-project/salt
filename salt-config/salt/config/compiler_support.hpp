f4e5006933ce8f53fc7c1e884667431fccdf3515
wander <kovacz@ukr.net>
2023-08-02 18:41:01 +0300

    Refactored config

--- salt-config/salt/config/compiler_support.hpp
+++ salt-config/salt/config/compiler_support.hpp
@@ -5,9 +5,7 @@
 //  clang defines the _MSC_VER and _MSC_FULL_VER macros.
 #if defined(_MSC_VER) && defined(SALT_CLANG)
 #    define SALT_MSC_FULL_VERSION (_MSC_FULL_VER)
-#elif defined(_MSC_VER) && !defined(SALT_CLANG)
-#    define SALT_MSC_FULL_VERSION (_MSC_FULL_VER)
-#elif defined(SALT_CLANG) && defined(__clang__)
+#elif defined(__clang__) && defined(SALT_CLANG)
 #    define SALT_CLANG_FULL_VERSION                                                                \
         (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
 #else
--- salt-config/salt/config/features_support.hpp
+++ salt-config/salt/config/features_support.hpp
@@ -28,18 +28,10 @@
 // MSVC implements [[no_unique_address]] as a silent no-op currently. If/when MSVC breaks its C++
 // ABI, it will be changed to work as intended. However, MSVC implements [[msvc::no_unique_address]]
 // which does what [[no_unique_address]] is supposed to do, in general.
-
-// Clang-cl does not yet (14.0) implement either [[no_unique_address]] or
-// [[msvc::no_unique_address]] though. If/when it does implement [[msvc::no_unique_address]], this
-// should be preferred though.
 #    define SALT_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
 #elif __has_cpp_attribute(no_unique_address)
 #    define SALT_NO_UNIQUE_ADDRESS [[no_unique_address]]
 #else
-// Note that this can be replaced by #error as soon as clang-cl implements msvc::no_unique_address,
-// since there should be no C++20 compiler that doesn't support one of the two attributes at that
-// point. I generally don't want to use this macro outside of C++20-only code, because using it
-// conditionally in one language version only would make the ABI inconsistent.
 #    define SALT_NO_UNIQUE_ADDRESS /* nothing */
 #endif
 
