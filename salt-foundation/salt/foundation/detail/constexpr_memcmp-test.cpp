62a70bfd14cfff1dcc75a31bd33a371b9bc2238a
wander <kovacz@ukr.net>
2023-08-06 22:19:15 +0300

    Updated unit tests

--- salt-foundation/salt/foundation/detail/constexpr_memcmp-test.cpp
+++ salt-foundation/salt/foundation/detail/constexpr_memcmp-test.cpp
@@ -5,16 +5,41 @@ constexpr unsigned char Banand[] = "Banand";
 constexpr unsigned char Banane[] = "Banane";
 constexpr unsigned char Bananf[] = "Bananf";
 
+constexpr bool true1[]  = {true, true, true};
+constexpr bool true2[]  = {true, true, true};
+constexpr bool false1[] = {false, false, false};
+
 using salt::fdn::detail::constexpr_memcmp;
 using salt::fdn::detail::constexpr_memcmp_equal;
 
 TEST_CASE("salt::fdn::detail::constexpr_memcmp", "[salt-foundation/constexpr_memcmp.hpp]") {
-    STATIC_REQUIRE(constexpr_memcmp(Banane, Banand, 6) == 1);
-    STATIC_REQUIRE(constexpr_memcmp(Banane, Banane, 6) == 0);
-    STATIC_REQUIRE(constexpr_memcmp(Banane, Bananf, 6) == -1);
+    SECTION("constant evaluated") {
+        STATIC_REQUIRE(constexpr_memcmp(Banane, Banand, 6) == 1);
+        STATIC_REQUIRE(constexpr_memcmp(Banane, Banane, 6) == 0);
+        STATIC_REQUIRE(constexpr_memcmp(Banane, Bananf, 6) == -1);
+    }
+
+    SECTION("basic") {
+        CHECK(constexpr_memcmp(Banane, Banand, 6) == 1);
+        CHECK(constexpr_memcmp(Banane, Banane, 6) == 0);
+        CHECK(constexpr_memcmp(Banane, Bananf, 6) == -1);
+    }
 }
 
 TEST_CASE("salt::fdn::detail::constexpr_memcmp_equal", "[salt-foundation/constexpr_memcmp_equal.hpp]") {
-    STATIC_REQUIRE_FALSE(constexpr_memcmp_equal(Banane, Banand, 6));
-    STATIC_REQUIRE(constexpr_memcmp_equal(Banane, Banane, 6));
+    SECTION("constant evaluated") {
+        STATIC_REQUIRE_FALSE(constexpr_memcmp_equal(Banane, Banand, 6));
+        STATIC_REQUIRE(constexpr_memcmp_equal(Banane, Banane, 6));
+
+        STATIC_REQUIRE_FALSE(constexpr_memcmp_equal(true1, false1, 3));
+        STATIC_REQUIRE(constexpr_memcmp_equal(true1, true2, 3));
+    }
+
+    SECTION("basic") {
+        CHECK_FALSE(constexpr_memcmp_equal(Banane, Banand, 6));
+        CHECK(constexpr_memcmp_equal(Banane, Banane, 6));
+
+        CHECK_FALSE(constexpr_memcmp_equal(true1, false1, 3));
+        CHECK(constexpr_memcmp_equal(true1, true2, 3));
+    }
 }
--- salt-foundation/salt/foundation/detail/constexpr_memcmp.hpp
+++ salt-foundation/salt/foundation/detail/constexpr_memcmp.hpp
@@ -7,7 +7,7 @@ namespace salt::fdn::detail {
 template <typename T, typename U> requires meta::trivially_lexicographically_comparable<T, U>
 constexpr int constexpr_memcmp(T const* lhs, U const* rhs, std::size_t count) noexcept {
     if consteval {
-        if (sizeof(T) == 1 && !meta::same_as<T, bool>) {
+        if (!meta::same_as<T, bool>) {
             // Death to compilers which do not support __builtin_memcmp.
             return __builtin_memcmp(lhs, rhs, count * sizeof(T));
         }
@@ -24,7 +24,7 @@ constexpr int constexpr_memcmp(T const* lhs, U const* rhs, std::size_t count) no
 template <typename T, typename U> requires meta::trivially_equality_comparable<T, U>
 constexpr bool constexpr_memcmp_equal(T const* lhs, U const* rhs, std::size_t count) noexcept {
     if consteval {
-        if (sizeof(T) == 1 && meta::integral<T> && !meta::same_as<T, bool>) {
+        if (meta::integral<T> && !meta::same_as<T, bool>) {
             // Death to compilers which do not support __builtin_memcmp.
             return __builtin_memcmp(lhs, rhs, count * sizeof(T)) == 0;
         }
